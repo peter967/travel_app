ff0367b187e57630333ca9600111ebf5a1557f0d
peter967 <pnapil96@gmail.com>
2023-11-16 02:13:51 +0200

    done app

--- lib/features/trip/data/datasources/trip_local_datasource.dart
+++ lib/features/trip/data/datasources/trip_local_datasource.dart
@@ -1,12 +1,21 @@
-import 'package:hive/hive.dart';
-import 'package:travel_app/features/trip/data/models/trip_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../models/trip_model.dart';
 
 class TripLocalDataSource {
   final Box<TripModel> tripBox;
 
   TripLocalDataSource(this.tripBox);
-  List<TripModel> getTrips() => tripBox.values.toList();
 
-  void addTrip(TripModel trip) => tripBox.add(trip);
-  void deleteTrip(int index) => tripBox.deleteAt(index);
+  List<TripModel> getTrips() {
+    return tripBox.values.toList();
+  }
+
+  void addTrip(TripModel trip) {
+    tripBox.add(trip);
+  }
+
+  void deleteTrip(int index) {
+    tripBox.deleteAt(index);
+  }
 }
--- lib/features/trip/presentation/pages/add_trip_screen.dart
+++ lib/features/trip/presentation/pages/add_trip_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:travel_app/features/trip/domain/entities/trip.dart';
-import 'package:travel_app/features/trip/presentation/providers/trip_provider.dart';
+
+import '../../domain/entities/trip.dart';
+import '../providers/trip_provider.dart';
 
 class AddTripScreen extends ConsumerWidget {
   final _formKey = GlobalKey<FormState>();
-  final _titleController = TextEditingController();
-  final _descController = TextEditingController();
-  final _locationController = TextEditingController();
-  final _pictureController = TextEditingController();
-  List<String> picture = [];
+  final _titleController = TextEditingController(text: "City 1");
+  final _descController = TextEditingController(text: "Best city ever");
+  final _locationController = TextEditingController(text: "Paris");
+  final _pictureController = TextEditingController(
+      text:
+          'https://images.unsplash.com/photo-1499856871958-5b9627545d1a?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D');
+  List<String> pictures = [];
 
   AddTripScreen({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Form(
@@ -22,19 +26,32 @@ class AddTripScreen extends ConsumerWidget {
             controller: _titleController,
             decoration: const InputDecoration(labelText: 'Title'),
           ),
+          TextFormField(
+            controller: _descController,
+            decoration: const InputDecoration(labelText: 'Description'),
+          ),
+          TextFormField(
+            controller: _locationController,
+            decoration: const InputDecoration(labelText: 'Location'),
+          ),
+          TextFormField(
+            controller: _pictureController,
+            decoration: const InputDecoration(labelText: 'Photo'),
+          ),
           ElevatedButton(
             onPressed: () {
-              picture.add(_pictureController.text);
+              pictures.add(_pictureController.text);
               if (_formKey.currentState!.validate()) {
                 final newTrip = Trip(
                   title: _titleController.text,
-                  photos: picture,
                   description: _descController.text,
                   date: DateTime.now(),
-                  location: _descController.text,
+                  location: _locationController.text,
+                  photos: pictures,
                 );
                 ref.read(tripListNotifierProvider.notifier).addNewTrip(newTrip);
-                ref.watch(tripListNotifierProvider.notifier).loadTrips();
+                ref.read(tripListNotifierProvider.notifier).loadTrips();
+                //Navigator.pop(context);
               }
             },
             child: const Text('Add Trip'),
